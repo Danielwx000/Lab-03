aba3bb40b401d82a92042d123c9f6cb4142b3bf2
Daniel Vintila <c00307041@setu.ie>
2025-09-22 15:28:47 +0000

    Updated code

--- Task-1.1.py
+++ Task-1.1.py
@@ -1,49 +1,38 @@
 # lab2-2_starter.py
-
+import re 
+ips = []
+counter = 0
 LOGFILE = "sample_auth_small.log"  # change filename if needed
 
-def simple_parser(line):
-    """
-    looks for the substring ' port ' and returns the following port number.
-    Returns None if no matching substring found.
-    """
-    if " port " in line:
-        parts = line.split() # splits the line into tokens, seperates by spaces by default
-        try:
-            anchor = parts.index("port")    # Find the position of the token "port", our anchor
-            port = parts[anchor+1]          # the port value will be next token, anchor+1
-            return port.strip()             # strip any trailing punctuation
-        
-        except (ValueError, IndexError):
-            return None
-        
-    return None
-
 def ip_parser(line):
-    """
-    looks for the substring ' port ' and returns the following port number.
-    Returns None if no matching substring found.
-    """
+   
     if " from " in line:
         parts = line.split() # splits the line into tokens, seperates by spaces by default
         try:
             anchor = parts.index("from")    # Find the position of the token "port", our anchor
             ip = parts[anchor+1]          # the port value will be next token, anchor+1
-            #return ip.strip()             # strip any trailing punctuation
-            iplist = print(ip.strip())
+            return ip.strip()             # strip any trailing punctuation
             
-
         except (ValueError, IndexError):
             return None
 
     return None
+with open('sample_auth_small.log', 'r') as f:
+    for line in f:
+        ip_pattern = r"\d+\.\d+\.\d+\.\d+"
+        found_ips = re.findall(ip_pattern,line)
+        for ip in found_ips:
+            ips.append(ip)
+            #counter += 1 
+unique_ips = set(ips)
+print("Uniquie Ip's ")
+for ip in unique_ips:
+    counter += 1
+    print(ip)
+
+print("The ammount of unique ip's is", counter)
 
 
 ## This is the main block that will run first. 
 ## It will call any functions from above that we might need.
-if __name__ == "__main__":
 
-    with open(LOGFILE, "r") as f:
-        for line in f:
-            print (ip_parser(line.strip()))
-            
