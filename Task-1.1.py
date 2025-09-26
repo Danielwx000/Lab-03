eb8a2b17ddaa31f6e4ef1ea0950387bfc6a889f2
Daniel Vintila <c00307041@setu.ie>
2025-09-26 08:33:27 +0000

    Finished task 1.2

--- Task-1.1.py
+++ Task-1.1.py
@@ -1,7 +1,8 @@
 # lab2-2_starter.py
 import re 
 ips = []
-counter = 0
+ipcounter = 0
+linecon = 0
 LOGFILE = "sample_auth_small.log"
 
 def ip_parser(line):
@@ -15,18 +16,27 @@ def ip_parser(line):
             
         except (ValueError, IndexError):
             return None
+        
+    return None
+
 
 with open('sample_auth_small.log', 'r') as f:
     for line in f:
+        linecon += 1
         ip_pattern = r"\d+\.\d+\.\d+\.\d+"
         found_ips = re.findall(ip_pattern,line)
         for ip in found_ips:
             ips.append(ip)
-            #counter += 1 
-unique_ips = set(ips)
-print("Uniquie Ip's ")
-for ip in unique_ips:
-    counter += 1
-    print(ip)
 
-print("The ammount of unique ip's is", counter)
+
+unique_ips = set(ips)
+sortedIps = sorted(unique_ips)
+#print("Uniquie Ip's ")
+for ip in unique_ips:
+    ipcounter += 1
+    #print(ip)
+    
+
+print("Lines read:  ", linecon)
+print("Unique Ips:  ", ipcounter)
+print("First 10 Ips: ", sortedIps[:10])
